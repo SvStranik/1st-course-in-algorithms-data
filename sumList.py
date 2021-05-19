807a5fc02104d19fbfd91dcc373f3837ba7d237c
Svyatoslav <77211900+SvStranik@users.noreply.github.com>
2021-05-19 16:11:07 +0300

    Update sumList.py

--- sumList.py
+++ sumList.py
@@ -1,7 +1,9 @@
 def sumList(s1,s2):
-    if len(s1) != len(s2): return
-    resultat = []
-    for i in range(len(s1)):
-        resultat.append(s1[i].value + s2[i].value)
-    return resultat
-  
+    if len(s1) == len(s2) and len(s1) >= 1:
+        node1, node2= s1[0],s2[0]
+        resultat = []
+        while node1:
+            resultat.append(node1.value + node2.value)
+            node1, node2 = node1.next,node2.next  
+        return resultat
+    return []
