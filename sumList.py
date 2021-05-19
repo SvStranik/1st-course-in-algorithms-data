103d35d55cbf7a08c469518fbd4efffec2fe9db6
Svyatoslav <77211900+SvStranik@users.noreply.github.com>
2021-05-19 18:06:48 +0300

    Update sumList.py

--- sumList.py
+++ sumList.py
@@ -1,9 +1,10 @@
 def sumList(s1,s2):
-    if s1.len() != s2.len(): return None
-    s3 = LinkedList()
-    node,node2 = s1.head,s2.head
-    while node:
-        per = node.value + node2.value
-        s3.add_in_tail(Node(per))
-        node,node2 = node.next,node2.next
-    return s3 
+    if s1.len() == s2.len() and s1.len() >= 1: 
+        s3 = LinkedList()
+        node,node2 = s1.head,s2.head
+        while node:
+            per = node.value + node2.value
+            s3.add_in_tail(Node(per))
+            node,node2 = node.next,node2.next
+        return s3   
+    return None
