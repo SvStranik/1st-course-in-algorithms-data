8c677fd8a5aed97fbdf1a04ef08e60a838dfccdf
Svyatoslav <SvStranik@gmail.com>
2021-05-22 21:34:54 +0300

    Update LinkedList2.py

--- LinkedList2.py
+++ LinkedList2.py
@@ -22,22 +22,97 @@ class LinkedList2:
         self.tail = item
 
     def find(self, val):
-        return None # здесь будет ваш код
+        node = self.head
+        while node:
+            if node.value == val: return node
+            node = node.next
+        return None 
 
     def find_all(self, val):
-        return [] # здесь будет ваш код
+        node = self.head
+        resultat = []
+        while node:
+            if node.value == val: 
+                resultat.append(node)
+            node = node.next
+        return resultat 
 
     def delete(self, val, all=False):
-        pass # здесь будет ваш код
+        if self.head == None: return
+        while self.head.value == val:
+            if self.head.next == None:
+                self.head = None
+                self.tail = None
+                return
+            self.head = self.head.next
+            if all != True: return
+        else:
+            self.prev = None
+        node = self.head
+        while self.tail != node:
+            if node.next.value == val:
+                if node.next.next == None:
+                    node.next = None
+                    self.tail = node
+                    return
+                node.next.next.prev = node
+                node.next = node.next.next
+                if all != True: return
+                continue
+            node = node.next
 
     def clean(self):
-        pass # здесь будет ваш код
+        self.head = None
+        self.tail = None 
 
     def len(self):
-        return 0 # здесь будет ваш код
+        resultat = 0
+        node = self.head
+        while node:
+            resultat += 1
+            node = node.next
+        return resultat 
 
     def insert(self, afterNode, newNode):
-        pass # здесь будет ваш код
+        node = self.head
+        if node == None:
+            self.head = newNode
+            self.tail = newNode
+            return
+        while node:
+            if node == afterNode:
+                if node.next == None:
+                    self.tail = newNode
+                    newNode.prev = node
+                else:
+                    node.next.prev = newNode
+                    newNode.next = node.next
+                    newNode.prev = node
+                node.next = newNode
+                return
+            if node.next == None and node != afterNode:
+                node.next = newNode
+                newNode.prev = node
+                self.tail = newNode
+                return
+            node = node.next
 
     def add_in_head(self, newNode):
-        pass # здесь будет ваш код
+        node = self.head
+        if node == None:
+            self.tail = newNode
+        else:
+            node.prev = newNode
+            newNode.next = node
+        self.head = newNode
+
+    def print_all_nodes(self):# УДАЛИТЬ
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+
+s_list1 = LinkedList2()
+#s_list1.add_in_tail(Node(2))
+
+print(s_list1.len())
