94b3163ce8c436b3c0be71725f2adc73d0370d49
Svyatoslav <SvStranik@gmail.com>
2021-06-01 13:27:39 +0300

    Update

--- Stack.py
+++ Stack.py
@@ -23,3 +23,34 @@ class Stack:
         if self.size() != 0:
             return self.stack[0]
         return None 
+
+def twoStach(s):
+    stack1 = Stack()
+    stack2 = Stack()
+    stack1.stack = s[:]
+    while stack1.peek():
+        if type(stack1.stack[0]) == int:
+            per = stack1.peek()
+        elif stack1.peek() == "+":
+            per = 0
+            while stack2.peek():
+                per += stack2.pop()
+        else:
+            per = 1
+            while stack2.peek():
+                per *= stack2.pop()
+        stack2.push(per)
+        stack1.pop()
+    return stack2.peek()
+
+def equalityParentheses(s):
+    stack = Stack()
+    stack.stack = s[:]
+    count = 0
+    while stack.peek():
+        if count >= 0:
+            if stack.pop() == "(": count += 1
+            else: count -= 1
+        else: return "не сбалансированы"
+    if count == 0 : return "сбалансированы"
+    else: return "не сбалансированы"
