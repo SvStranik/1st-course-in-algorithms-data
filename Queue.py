cd41c5eafaaa7db96acdea38fa7d20efe8d5c8c0
Svyatoslav <SvStranik@gmail.com>
2021-06-02 13:22:56 +0300

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,22 +1,47 @@
+class Node:
+
+    def __init__(self,v):
+        self.value = v
+        self.next = None 
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self,item): 
+        if self.head is None: self.head = item
+        else: self.tail.next = item
+        self.tail = item
+    
+    def delete(self):
+        if self.head == None: return None
+        per = self.head.value
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+        return per
+        
+    def len(self):
+        resultat = 0
+        node = self.head
+        while node is not None:
+            resultat += 1
+            node = node.next
+        return resultat
+
 class Queue:
     def __init__(self):
-        self.queue = []
+        self.queue = LinkedList()
 
     def enqueue(self, item):
-        queue = []
-        queue.append(item)
-        for i in range(self.size()):
-            queue.append(self.queue[i])
-        self.queue = queue
+        self.queue.add_in_tail(Node(item))
 
     def dequeue(self):
-        if self.size() != 0:
-            queue = self.queue[-1]
-            self.queue = self.queue[:-1]
-            return queue
-        return None 
+        return self.queue.delete()
 
     def size(self):
-        size = len(self.queue)
-        if size != 0: return size
-        return 0
+        return self.queue.len()
