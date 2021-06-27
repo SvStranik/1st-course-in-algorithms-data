dc33dff98e73e8d4ba15933788f1430376a08f31
Svyatoslav <SvStranik@gmail.com>
2021-06-27 15:25:33 +0300

    add

--- PowerSet.py
+++ PowerSet.py
@@ -1,76 +1,57 @@
-class HashTable:
-    def __init__(self, sz, stp):
+class PowerSet:
+
+    def __init__(self, sz = 100):
         self.Size = sz
-        self.step = stp
-        if self.Size % self.step == 0: self.step += 1
-        self.slots = [None] * self.Size
-        self.arr_slot = []
+        self.slots = [[] for _ in range(self.Size)]
+        self.sizeSlot = 0
 
 
     def hash_fun(self, value):
          return sum(value.encode('utf-8')) % self.Size
 
 
-    def seek_slot2(self, value, per):
-        arr = [None,value]
-        index_slot = self.hash_fun(value)
-        position = index_slot
-        bool = False
-        while position != index_slot or bool != True:
-            if self.slots[position] == arr[per]: return position
-            position += self.step
-            if position >= self.Size:
-                position -= self.Size
-                bool = True
-        return None
+    def seek_slot(self, value):
+        if self.size() == self.Size:
+            self.increaseSlots()
+        self.slots[self.hash_fun(value)].append(value)
+        self.sizeSlot += 1
 
 
-    def array_slot(self, value,add = True):
-        if add == True: return self.arr_slot.append(value)
-        index = self.arr_slot.index(value)
-        arr_slot = []
-        for i in range(len(self.arr_slot)):
-            if i == index: continue
-            arr_slot.append(self.arr_slot[i])
-        self.arr_slot = arr_slot
-
-
-class PowerSet(HashTable):
-
-
-    def __init__(self,sz = 25000,stp = 201):
-        super(PowerSet,self).__init__(sz,stp)
-
+    def increaseSlots(self):
+        self.Size *= 2
+        slots2 = [[] for _ in range(self.Size)]
+        for i in range(len(self.slots)):
+            for j in range(len(self.slots[i])):
+                slots2[self.hash_fun(self.slots[i][j])].append(self.slots[i][j])
+        self.slots = slots2
+            
 
     def size(self):
-        counter = 0
-        for i in self.slots:
-            if i != None:counter += 1
-        return counter
+        return self.sizeSlot
 
 
     def put(self, value):
-        if value not in self.arr_slot:
-            position_slot = self.seek_slot2(value,0)
-            self.slots[position_slot] = value
-            self.array_slot(value)
-        
+        if not self.get(value):
+            return self.seek_slot(value)
+
 
     def get(self, value):
-        index_slot = self.seek_slot2(value,1)
-        if index_slot == None:return False
-        else: return True
-        
+        for i in self.slots[self.hash_fun(value)]:
+            if i == value:
+                return True
+        return False
+
 
     def remove(self, value):
-        index_slot = self.seek_slot2(value,1)
-        if index_slot == None:
-            return False
-        else:
-            self.slots[index_slot] = None
-            self.array_slot(value,False)
-            return True
-
+        list_slot = self.slots[self.hash_fun(value)]
+        for i in range(len(list_slot)):
+            if list_slot[i] == value:
+                list_slot[i] = list_slot[len(list_slot)-1]
+                list_slot.pop()
+                self.sizeSlot -= 1
+                return True
+        return False
+ 
 
     def intersection(self, set2):
         resultat = []
@@ -83,27 +64,35 @@ class PowerSet(HashTable):
 
     def union(self, set2):
         resultat = []
-        for i in set2:
-            if self.get(i) == False:
-                resultat.append(i)
-        set1 = self.printPw()
-        for j in set1:
-            resultat.append(j)
+        for i in range(len(self.slots)):
+            for j in range(len(self.slots[i])):
+                resultat.append(self.slots[i][j])
+        for j in set2:
+            if not self.get(j):
+                resultat.append(j)
         if len(resultat)>0: return set(resultat)
         return None
 
 
     def difference(self, set2):
         resultat = []
-        set1 = self.printPw()
-        for j in set1:
-            if j not in set2:
-                resultat.append(j)
-        if len(resultat)>0: return set(resultat)
-        return None
+        for i in range(len(self.slots)):
+            for j in range(len(self.slots[i])):
+                if self.slots[i][j] not in  set2:
+                    resultat.append(self.slots[i][j])
+        return resultat
+
 
 
     def issubset(self, set2):
         for i in set2:
-            if self.get(i) == False: return False
+            if not self.get(i): return False
         return True
+
+
+    def printSlots(self):
+        resultat = []
+        for i in range(len(self.slots)):
+            for j in range(len(self.slots[i])):
+                resultat.append(self.slots[i][j])
+        return resultat
