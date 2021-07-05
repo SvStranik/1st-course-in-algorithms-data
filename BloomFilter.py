d2fd7ae2b188c115855e702acd4da3f50b5ced80
Svyatoslav <SvStranik@gmail.com>
2021-07-05 17:12:09 +0300

    edit

---
+++ BloomFilter.py
@@ -0,0 +1,35 @@
+class BloomFilter:
+
+    def __init__(self, f_len = 32):
+        self.filter_len = f_len
+        self.filter_arr =  [0] * self.filter_len
+
+
+    def hash1(self, str1):
+        resultat = 0
+        for c in str1:
+            code = ord(c)
+            resultat = (resultat * 17 + code) % self.filter_len
+        return resultat
+
+
+    def hash2(self, str1):
+        resultat = 0
+        for c in str1:
+            code = ord(c)
+            resultat = (resultat * 223 + code) % self.filter_len
+        return resultat
+
+
+    def add(self, str1):
+        if isinstance(str1, str):
+            self.filter_arr[self.hash1(str1)] = 1
+            self.filter_arr[self.hash2(str1)] = 1
+
+
+    def is_value(self, str1):
+        if isinstance(str1, str):
+            if self.filter_arr[self.hash1(str1)] == 0: return False
+            if self.filter_arr[self.hash2(str1)] == 0: return False
+            return True
+
