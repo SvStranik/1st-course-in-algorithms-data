381cd9a122199e753ecb7ef63f7121c1f46ff96e
Svyatoslav <SvStranik@gmail.com>
2021-07-14 15:43:22 +0300

    edit

--- LinkedList2Dop.py
+++ LinkedList2Dop.py
@@ -1,107 +1,45 @@
 class Node:
+
     def __init__(self,v):
         self.value = v
         self.prev = None
         self.next = None 
 
 class DummyNode:
-    
     def __init__(self):
-        self.prev = None
-        self.next = None
+        self.head = Node("head")
+        self.tail = Node("tail")
+    
+class LinkedList2(DummyNode):  
 
-class LinkedList2: 
-    
     def __init__(self):
-        self.head = None
-        self.tail = None
+        super().__init__()
         
-    def delete(self,val, all = False):
-        self.add_in_head(DummyNode())
-        self.add_in_tail(DummyNode())
-        node = self.head
-        while node.next:
-            node = node.next
-            if node.value ==val:
-                node.next.prev = node.prev
-                node.prev.next = node.next
-                if all == False: break
-        if self.head.next == self.tail:
-            self.head = None
-            self.tail = None
-        else:
-            self.head = self.head.next
-            self.head.prev = None
-            self.tail = self.tail.prev
-            self.tail.next = None     
-            
     def add_in_tail(self, item):
-        if self.head is None:
-            self.head = item
-            item.prev = None
-            item.next = None
+        head = self.head
+        tail = self.tail
+        if head.next == None:
+            head.next = item
+            item.prev = head
+            tail.prev = item
         else:
-            self.tail.next = item
-            item.prev = self.tail
-        self.tail = item
-
-    def find(self, val):
-        node = self.head
-        while node:
-            if node.value == val: return node
+            item.prev = tail.prev
+            tail.prev.next = item
+            tail.prev = item
+        item.next = tail
+        
+        
+    def delete(self, val, all=False):
+        head = self.head
+        tail = self.tail
+        while head != tail:
+            if head.value == val:
+                head.prev.next = head.next
+                head.next.prev = head.prev
+            head = head.next
+    
+    def print_all(self):
+        node = self.head.next
+        while node.next != None:
+            print(node.value)
             node = node.next
-        return None 
-
-    def find_all(self, val):
-        node = self.head
-        resultat = []
-        while node:
-            if node.value == val: 
-                resultat.append(node)
-            node = node.next
-        return resultat 
-
-    def clean(self):
-        self.head = None
-        self.tail = None 
-
-    def len(self):
-        resultat = 0
-        node = self.head
-        while node:
-            resultat += 1
-            node = node.next
-        return resultat 
-
-    def insert(self, afterNode, newNode):
-        node = self.head
-        if node == None:
-            self.head = newNode
-            self.tail = newNode
-            return
-        while node:
-            if node == afterNode:
-                if node.next == None:
-                    self.tail = newNode
-                    newNode.prev = node
-                else:
-                    node.next.prev = newNode
-                    newNode.next = node.next
-                    newNode.prev = node
-                node.next = newNode
-                return
-            if node.next == None and node != afterNode:
-                node.next = newNode
-                newNode.prev = node
-                self.tail = newNode
-                return
-            node = node.next
-
-    def add_in_head(self, newNode):
-        node = self.head
-        if node == None:
-            self.tail = newNode
-        else:
-            node.prev = newNode
-            newNode.next = node
-        self.head = newNode
