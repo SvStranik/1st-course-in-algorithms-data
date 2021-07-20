33724f2f63058934e53187b96bad16bfd1820a68
Svyatoslav <SvStranik@gmail.com>
2021-07-20 14:06:18 +0300

    edit

--- LinkedList2Dop.py
+++ LinkedList2Dop.py
@@ -2,42 +2,47 @@ class Node:
 
     def __init__(self,v):
         self.value = v
+        self.next = None
         self.prev = None
-        self.next = None 
-
+         
+    
+    def get_next(self,node):
+        self.next = node
+   
+    def get_prev(self,node):
+        self.prev = node
+        
+    def node_prev(self):
+        return self.prev
+        
+    def node_next(self):
+        return self.next
+        
 class DummyNode(Node):
     def __init__(self,v):
-        super(DummyNode,self).__init__(v)
-        self.get_next = None
-        self.get_prev = None
-        
-        
+        super().__init__(v)
+
 class LinkedList2:  
 
     def __init__(self):
-        self.head = DummyNode("head")
-        self.tail = DummyNode("tail")
-        self.head.next = self.tail
+        self.head = DummyNode('Head')
+        self.tail = DummyNode('Tail')
         self.tail.prev = self.head
-        
-    def add_in_tail(self, node):
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev.next = node
-        self.tail.prev = node
-        
-        
-        
-    def delete(self, val, all=False):
-        node = self.head
-        while node:
-            if node.value == val:
-                node.prev.next = node.next
-                node.next.prev = node.prev
-            node = node.next
+        self.head.next = self.tail
+   
     
-    def print_all(self):
-        node = self.head.next
-        while node.next != None:
-            print(node.value)
-            node = node.next
+    def add_in_tail(self, newNode):
+        noda = self.tail
+        noda2 = noda.node_prev()
+        noda.get_prev(newNode)
+        newNode.get_next(noda)
+        noda2.get_next(newNode)
+        newNode.get_prev(noda2)
+        
+    def delete(self, val):
+        noda = self.head
+        while noda:
+            if noda.value == val:
+                noda.node_prev().get_next(noda.node_next())
+                noda.node_next().get_prev(noda.node_prev())
+            noda = noda.node_next()
